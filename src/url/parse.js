e728c0ab60b41156a45c60f4f813cd4ae55426ea
mmzhou <mmzhou@tencent.com>
2017-02-12 17:53:13 +0800

    Fixed parse function;

--- src/url/parse.js
+++ src/url/parse.js
@@ -2,30 +2,59 @@ var startWith = function (str, part) {
     return str.indexOf(part) === 0;
 };
 
+var DEFAULT_PORT_MAP = {
+    ftp: 21,
+    file: 0,
+    gopher: 70,
+    http: 80,
+    https: 443,
+    ws: 80,
+    wss: 443
+};
+
 /**
  * parse url.
  * @param {string} url url string to be parsed.
  * @param {string=} relativedUrl relatived url, must be absolute url.
  * @return {Object} parsed url.
+ *           'http://example.com:8000/books/12?type=music#hash'
  *           {
- *              protocol: '',
- *              host: '',
- *              port: '',
- *              path: '',
- *              query: '',
- *              url: ''
+ *              protocol: 'http:',
+ *              origin: 'http://example.com:8000'
+ *              host: 'example.com:8000',
+ *              hostname: 'example.com',
+ *              port: '8000',
+ *              pathname: '/books/12',
+ *              search: '?type=music',
+ *              href: 'http://example.com:8000/books/12?type=music#hash'，
+ *              hash: '#hash'
  *           }
  */
 var parseUrl = function (url, relativedUrl) {
     function parseLink(link) {
-        var hostname = (link.hostname || '').split(':')[0].toLowerCase();
+        var hostname = (link.hostname || '').toLowerCase();
         var protocol = (link.protocol || '').toLowerCase();
-        var port = link.port * 1 || (protocol === 'http:' ? 80 : (protocol === 'https:' ? 443 : ''));
+        var ptl = protocol.slice(0, -1);
+        var port = (link.port || DEFAULT_PORT_MAP[ptl] || 0) + '';
         var pathname = link.pathname || '';
         if (!startWith(pathname, '/')) {
             pathname = '/' + pathname;
         }
-        return [hostname, '' + port, pathname];
+        // only show port when it's not default port.
+        var useDefaultPort = (DEFAULT_PORT_MAP[ptl] + '') === port;
+        var host = hostname + (useDefaultPort  ? '' : (':' + port));
+
+        return {
+            host: host,
+            hostname: hostname,
+            port: port,
+            pathname: pathname,
+            search: link.search || '',
+            protocol: protocol,
+            hash: link.hash || '',
+            href: link.href || '',
+            origin: protocol + '//' + host
+        };
     }
 
     var doc = document;
@@ -33,46 +62,39 @@ var parseUrl = function (url, relativedUrl) {
 
     // parse relatived url
     link.href = relativedUrl || doc.location.href;
-    var relativedProtocol = (link.protocol || '').toLowerCase();
     var relatived = parseLink(link);
 
     // if url is relative, then convert it into absolute url.
-    if (startWith(url, '//')) {
-        // url === '//qidian.qq.com/da/i.js?pid=1123123'
-        url = relativedProtocol + url;
+    if (!url) {
+        // url === ''
+        url = relatived.href;
     }
-    else {
-        var relativedBaseUrl = relativedProtocol + '//' + relatived[0] + (relatived[1] ? ':' + relatived[1] : '');
-        if (startWith(url, '/')) {
-            // url === '/da/i.js'
-            url = relativedBaseUrl + url;
-        }
-        else {
-            if (!url || startWith(url, '?')) {
-                // url === ''
-                // url === '?pid=1123123'
-                url = relativedBaseUrl + relatived[2] + (url || link.search || '');
-            }
-            else if (url.split('/')[0].indexOf(':') < 0) {
-                // url is relative path
-                // url === 'da/i.js'
-                url = relativedBaseUrl + relatived[2].substring(0, relatived[2].lastIndexOf('/')) + '/' + url;
-            }
-        }
+    else if (startWith(url, '//')) {
+        // url === '//qidian.qq.com/da/i.js?pid=1123123'
+        url = relatived.protocol + url;
+    }
+    else if (startWith(url, '?')) {
+        // url === '?pid=1123123'
+        url = relatived.origin + relatived.pathname + url;
+    }
+    else if (startWith(url, '#')) {
+        // url === '#hash'
+        url = relatived.origin + relatived.pathname + relatived.search + url;
+    }
+    else if (startWith(url, '/')) {
+        // url === '/da/i.js'
+        url = relatived.origin + url;
+    }
+    else if (url.split('/')[0].indexOf(':') < 0) {
+        // url is relative path
+        // url === 'da/i.js'
+        // url === './da/i.js'
+        // url === '../da/i.js'
+        url = relatived.origin + relatived.pathname.substring(0, relatived.pathname.lastIndexOf('/')) + '/' + url;
     }
 
     link.href = url;
-    var r = parseLink(link);
-    var protocol = (link.protocol || '').toLowerCase();
-    var query = link.search || '';
-    return {
-        protocol: protocol,
-        host: r[0],
-        port: r[1],
-        path: r[2],
-        query: query,
-        url: protocol + '//' + r[0] + ':' + r[1] + r[2] + query
-    };
+    return parseLink(link);
 };
 
 export default parseUrl;
--- test/url/parse.js
+++ test/url/parse.js
@@ -2,47 +2,159 @@ import parse from 'S3/url/parse';
 
 describe('S3/url/parse', function() {
     it('parse(absoluteUrl)', function () {
-        var url = parse('https://example.com:80/books/123/?a=1&b=3#hashid');
+        var url = parse('wss://example.com');
+        expect(url.origin).toBe('wss://example.com');
         expect(url.host).toBe('example.com');
-        expect(url.path).toBe('/books/123/');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/');
+        expect(url.port).toBe('443');
+        expect(url.protocol).toBe('wss:');
+        expect(url.search).toBe('');
+        expect(url.hash).toBe('');
+        expect(url.href).toBe('wss://example.com/');
+        var url = parse('https://example.com:80/books/123/?a=1&b=3#hashid');
+        expect(url.origin).toBe('https://example.com:80');
+        expect(url.host).toBe('example.com:80');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/books/123/');
         expect(url.port).toBe('80');
         expect(url.protocol).toBe('https:');
-        expect(url.query).toBe('?a=1&b=3');
-        expect(url.url).toBe('https://example.com:80/books/123/?a=1&b=3');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.hash).toBe('#hashid');
+        expect(url.href).toBe('https://example.com:80/books/123/?a=1&b=3#hashid');
         var url = parse('https://example.com/books/123/?a=1&b=3#hashid');
         expect(url.port).toBe('443');
         var url = parse('http://example.com/books/123/?a=1&b=3#hashid');
         expect(url.port).toBe('80');
-        expect(url.url).toBe('http://example.com:80/books/123/?a=1&b=3');
+        expect(url.href).toBe('http://example.com/books/123/?a=1&b=3#hashid');
         var url = parse('https://example.com:8000/books/123/?a=1&b=3#hashid');
+        expect(url.hostname).toBe('example.com');
+        var url = parse('ws://example.com/books/123?a=1&b=3#hashid');
+        expect(url.port).toBe('80');
         expect(url.host).toBe('example.com');
+        expect(url.pathname).toBe('/books/123');
+        expect(url.search).toBe('?a=1&b=3');
     });
 
-    it('parse(relativeUrl)', function () {
+    it('parse(\'books/123/?a=1&b=3#hashid\')', function () {
         var url = parse('books/123/?a=1&b=3#hashid');
-        expect(url.host).toBe('localhost');
-        expect(url.path).toBe('/books/123/');
+        expect(url.origin).toBe('http://localhost:9876');
+        expect(url.host).toBe('localhost:9876');
+        expect(url.hostname).toBe('localhost');
+        expect(url.pathname).toBe('/books/123/');
         expect(url.port).toBe('9876');
         expect(url.protocol).toBe('http:');
-        expect(url.query).toBe('?a=1&b=3');
-        expect(url.url).toBe('http://localhost:9876/books/123/?a=1&b=3');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.hash).toBe('#hashid');
+        expect(url.href).toBe('http://localhost:9876/books/123/?a=1&b=3#hashid');
+    });
+
+    it('parse(\'./books/123/?a=1&b=3#hashid\')', function () {
+        var url = parse('./books/123/?a=1&b=3#hashid');
+        expect(url.origin).toBe('http://localhost:9876');
+        expect(url.host).toBe('localhost:9876');
+        expect(url.hostname).toBe('localhost');
+        expect(url.pathname).toBe('/books/123/');
+        expect(url.port).toBe('9876');
+        expect(url.protocol).toBe('http:');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.hash).toBe('#hashid');
+        expect(url.href).toBe('http://localhost:9876/books/123/?a=1&b=3#hashid');
+    });
+
+    it('parse(\'../1?a=1&b=3#hashid\', \'http://example.com/books/123/\')', function () {
+        var url = parse('../1?a=1&b=3#hashid', 'http://example.com/books/123/');
+        expect(url.origin).toBe('http://example.com');
+        expect(url.host).toBe('example.com');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/books/1');
+        expect(url.port).toBe('80');
+        expect(url.protocol).toBe('http:');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.hash).toBe('#hashid');
+        expect(url.href).toBe('http://example.com/books/1?a=1&b=3#hashid');
+    });
+
+    it('parse(\'../../1?a=1&b=3#hashid\', \'http://example.com/books/love/123\')', function () {
+        var url = parse('../../1?a=1&b=3#hashid', 'http://example.com/books/love/123/');
+        expect(url.origin).toBe('http://example.com');
+        expect(url.host).toBe('example.com');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/books/1');
+        expect(url.port).toBe('80');
+        expect(url.protocol).toBe('http:');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.hash).toBe('#hashid');
+        expect(url.href).toBe('http://example.com/books/1?a=1&b=3#hashid');
+    });
+
+    it('parse(\'?a=1&b=3#hashid\', \'http://example.com\')', function () {
+        var url = parse('?a=1&b=3#hashid', 'http://example.com');
+        expect(url.origin).toBe('http://example.com');
+        expect(url.host).toBe('example.com');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/');
+        expect(url.port).toBe('80');
+        expect(url.protocol).toBe('http:');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.hash).toBe('#hashid');
+        expect(url.href).toBe('http://example.com/?a=1&b=3#hashid');
+    });
+
+    it('parse(\'#hashid\')', function () {
+        var url = parse('#hashid', 'http://example.com/books/123');
+        expect(url.origin).toBe('http://example.com');
+        expect(url.host).toBe('example.com');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/books/123');
+        expect(url.port).toBe('80');
+        expect(url.protocol).toBe('http:');
+        expect(url.search).toBe('');
+        expect(url.hash).toBe('#hashid');
+        expect(url.href).toBe('http://example.com/books/123#hashid');
+    });
+
+    it('parse(\'.\', \'http://example.com/books/123\')', function () {
+        var url = parse('.', 'http://example.com/books/123');
+        expect(url.origin).toBe('http://example.com');
+        expect(url.host).toBe('example.com');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/books/');
+        expect(url.port).toBe('80');
+        expect(url.protocol).toBe('http:');
+        expect(url.search).toBe('');
+        expect(url.hash).toBe('');
+        expect(url.href).toBe('http://example.com/books/');
+    });
+
+    it('parse(\'.\', \'http://example.com/books/123/\')', function () {
+        var url = parse('.', 'http://example.com/books/123/');
+        expect(url.origin).toBe('http://example.com');
+        expect(url.host).toBe('example.com');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/books/123/');
+        expect(url.port).toBe('80');
+        expect(url.protocol).toBe('http:');
+        expect(url.search).toBe('');
+        expect(url.hash).toBe('');
+        expect(url.href).toBe('http://example.com/books/123/');
     });
 
     it('parse(relativeUrl, baseUrl)', function () {
         var url = parse('books/123/?a=1&b=3#hashid', 'https://example.com/user/123/');
-        expect(url.host).toBe('example.com');
-        expect(url.path).toBe('/user/123/books/123/');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/user/123/books/123/');
         expect(url.port).toBe('443');
         expect(url.protocol).toBe('https:');
-        expect(url.query).toBe('?a=1&b=3');
-        expect(url.url).toBe('https://example.com:443/user/123/books/123/?a=1&b=3');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.href).toBe('https://example.com/user/123/books/123/?a=1&b=3#hashid');
 
         var url = parse('../../books/123/?a=1&b=3#hashid', 'https://example.com/user/123/');
-        expect(url.host).toBe('example.com');
-        expect(url.path).toBe('/books/123/');
+        expect(url.hostname).toBe('example.com');
+        expect(url.pathname).toBe('/books/123/');
         expect(url.port).toBe('443');
         expect(url.protocol).toBe('https:');
-        expect(url.query).toBe('?a=1&b=3');
-        expect(url.url).toBe('https://example.com:443/books/123/?a=1&b=3');
+        expect(url.search).toBe('?a=1&b=3');
+        expect(url.href).toBe('https://example.com/books/123/?a=1&b=3#hashid');
     });
 });
